ansible/awx

Fix ansible-lint indentation in module docstrings

awx_collection/plugins/lookup/schedule_rrule.py
@@ -73,9 +73,9 @@ DOCUMENTATION = """
 """
 
 EXAMPLES = """
-    - name: Create a string for a schedule
-      debug:
-        msg: "{{ lookup('awx.awx.schedule_rrule', 'none', start_date='1979-09-13 03:45:07') }}"
+- name: Create a string for a schedule
+  debug:
+    msg: "{{ lookup('awx.awx.schedule_rrule', 'none', start_date='1979-09-13 03:45:07') }}"
 """
 
 RETURN = """

awx_collection/plugins/lookup/schedule_rruleset.py
@@ -107,17 +107,17 @@ DOCUMENTATION = """
 """
 
 EXAMPLES = """
-    - name: Create a ruleset for everyday except Sundays
-      set_fact:
-        complex_rule: "{{ lookup(awx.awx.schedule_rruleset, '2022-04-30 10:30:45', rules=rrules, timezone='UTC' ) }}"
-      vars:
-        rrules:
-          - frequency: 'day'
-            interval: 1
-          - frequency: 'day'
-            interval: 1
-            byweekday: 'sunday'
-            include: False
+- name: Create a ruleset for everyday except Sundays
+  set_fact:
+    complex_rule: "{{ lookup(awx.awx.schedule_rruleset, '2022-04-30 10:30:45', rules=rrules, timezone='UTC' ) }}"
+  vars:
+    rrules:
+      - frequency: 'day'
+        interval: 1
+      - frequency: 'day'
+        interval: 1
+        byweekday: 'sunday'
+        include: False
 """
 
 RETURN = """

antuelle78/AWXai

awx_collection/plugins/lookup/schedule_rrule.py
@@ -73,9 +73,9 @@ DOCUMENTATION = """
 """
 
 EXAMPLES = """
-    - name: Create a string for a schedule
-      debug:
-        msg: "{{ query('awx.awx.schedule_rrule', 'none', start_date='1979-09-13 03:45:07') }}"
+- name: Create a string for a schedule
+  debug:
+    msg: "{{ query('awx.awx.schedule_rrule', 'none', start_date='1979-09-13 03:45:07') }}"
 """
 
 RETURN = """

awx_collection/plugins/lookup/schedule_rruleset.py
@@ -107,17 +107,17 @@ DOCUMENTATION = """
 """
 
 EXAMPLES = """
-    - name: Create a ruleset for everyday except Sundays
-      set_fact:
-        complex_rule: "{{ query(awx.awx.schedule_rruleset, '2022-04-30 10:30:45', rules=rrules, timezone='UTC' ) }}"
-      vars:
-        rrules:
-          - frequency: 'day'
-            interval: 1
-          - frequency: 'day'
-            interval: 1
-            byweekday: 'sunday'
-            include: False
+- name: Create a ruleset for everyday except Sundays
+  set_fact:
+    complex_rule: "{{ query(awx.awx.schedule_rruleset, '2022-04-30 10:30:45', rules=rrules, timezone='UTC' ) }}"
+  vars:
+    rrules:
+      - frequency: 'day'
+        interval: 1
+      - frequency: 'day'
+        interval: 1
+        byweekday: 'sunday'
+        include: False
 """
 
 RETURN = """